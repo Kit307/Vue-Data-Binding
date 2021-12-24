1b0b2d86cca39bc60ea25e17fcc92186e64f30d2
Kit307 <kintlove555@gmail.com>
2021-12-24 20:52:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### reference by CONCEPT
+### link https://github.com/puikinsh/concept
+edit by krit saeeaw 6304101307
---
+++ concept-master/documentation/js/kitvue.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Kit Saeeaw',
+        address: '410 หมู่ 2 ถ. รอบเมืองเชียงใหม่ ตำบล หนองจ๊อม อำเภอสันทราย เชียงใหม่ 50210',
+        birtdate : ' 5 November, 2001',
+        age: '20 Year Old',
+        major:'Computer Science',
+        faculty:'Science',
+        level:'sophomore ( 2 )',
+        gpa:'3.70',
+        num1: '43.75',
+        num2: '56.25',
+        unsuccessful: 'Unsuccessful',
+        succeed: 'Succeed',
+        language:['Programming Language Skill','C, C++','JS','Python','JS','JAVA','CSS']
+
+
+    }
+})
